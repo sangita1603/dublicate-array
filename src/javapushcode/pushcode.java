6501b143a7346db75a2efd0d955c580709b11ead
swapnilNJ <swapnil.nuron@gmail.com>
2022-09-25 00:04:44 +0530

    high priority code

---
+++ src/javapushcode/pushcode.java
@@ -0,0 +1,49 @@
+package javapushcode;
+
+public class pushcode {
+	public static void main(String[]argd)
+	{
+		arrayLength();
+		arrayaccending();
+	}
+	
+		public static void arrayLength()
+		{
+			int []array= {1,1,2,2,3,4,5,6};
+			{
+				System.out.println(array.length);
+				}}
+
+public static void arrayaccending()
+{
+	int []array= {2,1,3,9,5,1,2};
+	
+	for(int i=0; i<array.length-1;i++)
+	{
+		System.out.println(" "+array[i]);
+			
+	}}}
+
+/*
+	
+}nt[] arr= {12,11,67,90,34};
+int c=0;
+System.out.println("Third Higest Array Value");
+
+for(int i=0;i<arr.length;i++)
+{
+	for(int j=i+1;j<arr.length;j++)
+	{
+		if(arr[i]<arr[j]) 
+		{
+			c=arr[i];
+			arr[i]=arr[j];
+			arr[j]=c;					
+		}
+	}
+}
+System.out.println(arr[2]);
+}
+*/
+
+
